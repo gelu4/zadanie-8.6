49760bae8a0bfe1ff71a8081db18e04ec1350deb
Grzegorz Koza <kozagrzegorz@interia.pl>
2017-11-30 00:33:16 +0100

    add value check

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 var a = prompt('Write value a:');
 var b = prompt('Write value b:');
+
 var value = ((a*a) - (2*a*b) + (b*b));
 
 function setResult(result) { 
 	document.getElementById("wynik").innerHTML = result;
-}
-	 
+
 	if (value>0) {
 	setResult( value + '-Wynik dodatni');
 	}
@@ -20,4 +20,10 @@ function setResult(result) {
 	else { 
 		setResult('Musisz wpisać dwie wartości liczbowe');
 	}
-
+}
+if(a || b != "") {
+	setResult();
+}
+else{
+	alert("Wpisz w końcu te liczby!");
+}
