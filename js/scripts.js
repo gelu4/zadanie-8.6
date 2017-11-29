0efde192bfe2215684110981df50ddc1c7d1b563
Grzegorz Koza <kozagrzegorz@interia.pl>
2017-11-29 12:13:35 +0100

    Add setResult

--- js/scripts.js
+++ js/scripts.js
@@ -3,45 +3,21 @@ var b = prompt('Write value b:');
 var value = ((a*a) - (2*a*b) + (b*b));
 
 function setResult(result) { 
-	var result = document.getElementById("wynik").innerHTML;
-	return result;
+	document.getElementById("wynik").innerHTML = result;
 }
+	 
 	if (value>0) {
-	setResult(result) +  value + '-Wynik dodatni';
+	setResult( value + '-Wynik dodatni');
 	}
 	else if (value<0) {
-	setResult(result) +  value + '  -Wynik ujemny  ';
+	setResult(value + '  -Wynik ujemny  ');
 	}
 
-	else if (value== '0'){
-	setResult(result) +  value + '  -Zero  ';
+	else if (value === 0){
+	setResult(value + '  -Zero  ');
 	}
-
+	
 	else { 
-		setResult(result) + 'Musisz wpisać dwie wartości liczbowe';
+		setResult('Musisz wpisać dwie wartości liczbowe');
 	}
 
-/*function setResult() {
-	var result;
-	result= calculate(a, b);
-	
-	if (result >0) {
-	document.write(result+ '  -Wynik dodatni  ');
-	}
-
-	else if (result <0) {
-	document.write(result + '  -Wynik ujemny  ');
-	}
-	
-	else if (result ===0){
-	document.write(result + '  -Zero  ');
-	}
-	else if (result ===isNaN){
-	document.write(result + '  -ro  ');
-	}
-	
-	else
-	{  document.write('Musisz wpisać dwie wartości liczbowe');
-	}
-	
-}*/
